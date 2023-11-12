fbb2f81157e4bc98359b26c603ba330b4071f3c4
Louis de Forcrand <olius@bluewin.ch>
2023-11-12 01:44:01 +0100

    Add non-zero vertex check

--- solve.c
+++ solve.c
@@ -13,9 +13,20 @@ static int try(const size_t k, tiledef ds[k], face fs[20], face *f)
 		f->t->q--;
 		for (f->r = 0; f->r < 3; f->r++) {
 			int c;
-			for (c = 0; c < 3; c++)
-				if ((f->vs[c]->n -= f->t->n[(c+f->r)%3]) < 0)
+			for (c = 0; c < 3; c++) {
+				vertex *v = f->vs[c];
+				v->n -= f->t->n[(c+f->r)%3];
+				if (v->n > 0)
+					continue;
+				if (v->n < 0)
 					break;
+				int i;
+				for (i = 0; i < 5; i++)
+					if (v->fs[i] == NULL)
+						break;
+				if (i < 5)
+					break;
+			}
 			if (c == 3 && c-- && try(k,ds,fs,f+1))
 				return 1;
 			for (; c >= 0; c--)
@@ -33,8 +44,9 @@ int solve(size_t k, tiledef ds[k], vertex vs[12], face fs[20])
 		ifs[i] = vs[i].fs;
 	for (face *f = fs; f < fs+20; f++)
 		for (int c = 0; c < 3; c++) {
-			f->vs[c] = vs + corners[f-fs][c];
 			*ifs[corners[f-fs][c]]++ = f;
+			f->vs[c] = vs + corners[f-fs][c];
+			f->t = NULL;
 		}
 	return try(k,ds,fs,fs);
 }
